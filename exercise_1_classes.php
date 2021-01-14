36ae2638667ce55dffdc399e2180a24520e32890
Valerie <akpotevalerie@gmail.com>
2021-01-14 11:57:22 +0100

    Add solved exercise

--- exercise_1_classes.php
+++ exercise_1_classes.php
@@ -1,15 +1,37 @@
-  <?php
+<?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-/* EXERCISE 1
-Create a class beverage.
-Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
-Have a default value "cold" in the construct for temperature.
-Remember for now we will use properties and methods that can be accessed from everywhere.
-Make a getInfo function which returns "This beverage is <temperature> and <color>."
-Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
- print the getInfo on the screen.
-print the temperature on the screen.
-USE TYPEHINTING EVERYWHERE!
-*/
+    
+
+    /* EXERCISE 1
+    Create a class beverage.
+    Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
+    Have a default value "cold" in the construct for temperature.
+    Remember for now we will use properties and methods that can be accessed from everywhere.
+    Make a getInfo function which returns "This beverage is <temperature> and <color>."
+    Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
+    print the getInfo on the screen.
+    print the temperature on the screen.
+    USE TYPEHINTING EVERYWHERE!
+    */
+
+    class Beverage {
+
+        public $color, $price, $temperature;
+
+        function __construct(string $color, float $price) {
+            $this->color = $color;
+            $this->price = $price;
+            $this->temperature = "cold";
+        }
+
+        function getInfo() {
+            return "This beverage is $this->color and $this->temperature.<br>";
+        }
+    }
+
+    $cola = new Beverage("black", 2);
+
+    echo $cola->getInfo();
+    echo $cola->temperature;
