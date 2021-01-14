92e6ba2f9ce1eb13db9456d75538a089b3ea9a7d
Valerie <akpotevalerie@gmail.com>
2021-01-14 12:51:21 +0100

    Add files to index

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 
     require "exercise_1_classes.php";
     require "exercise_2_extending.php";
+    require "exercise_3_private.php";
+    require "exercise_4_protected.php";
+    require "exercise_5_public.php";
+    require "exercise_6_const.php";
+    require "exercise_7_static.php";
+
 
     function new_exercise($x) {
         $block = "<hr/><strong>Exercise $x:</strong><br/>";
