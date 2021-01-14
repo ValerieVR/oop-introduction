262e6957eadeec90338cb49e79e20801e640131c
Valerie <akpotevalerie@gmail.com>
2021-01-14 23:51:39 +0100

    Add solution

--- exercise_1_classes.php
+++ exercise_1_classes.php
@@ -13,8 +13,28 @@ Have a default value "cold" in the construct for temperature.
 Remember for now we will use properties and methods that can be accessed from everywhere.
 Make a getInfo function which returns "This beverage is <temperature> and <color>."
 Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
- print the getInfo on the screen.
+print the getInfo on the screen.
 print the temperature on the screen.
 USE TYPEHINTING EVERYWHERE!
 */
+
+class Beverage {
+
+    public $color, $price, $temperature;
+
+    public function __construct(string $color, float $price, string $temperature = "cold") {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo() :string {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+}
+
+$cola = new Beverage('black', 2);
+echo $cola->getInfo();
+echo "<br>";
+echo $cola->temperature;
 
