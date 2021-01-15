b8aad0ac04e55a9526c17b06900819e0ead6d0c8
Valerie <akpotevalerie@gmail.com>
2021-01-15 11:36:58 +0100

    Add solution

--- exercise_7_static.php
+++ exercise_7_static.php
@@ -13,3 +13,63 @@ which has the value "Melkmarkt 9, 2000 Antwerpen".
 print the address without creating a new instant of the beverage class 2 times in two different ways.
 Use typehinting everywhere!
 */
+
+class Beverage {
+
+    private $color, $price, $temperature;
+    const BARNAME = "Het Vervolg";
+    private static $address = "Melkmarkt 9, 2000 Antwerpen";
+
+    public function __construct(string $color, float $price, string $temperature = "cold") {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo() :string {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+
+    public function getConst() :string {
+        return "Deel 1: " . self::BARNAME;
+    }
+
+    public static function getAddress() {
+        return self::$address;
+    }
+   
+}
+
+class Beer extends Beverage {
+    private $name, $alcoholpercentage;
+
+    public function __construct(string $name, float $alcoholpercentage, string $color, float $price) {
+        parent::__construct($color, $price);
+        $this->name = $name;
+        $this->alcoholpercentage = $alcoholpercentage;
+    }
+
+    public function getAlcoholpercentage() {
+        return $this->alcoholpercentage . "%";
+    }
+
+    public function message() :string {
+        return "Deel 2: " . self::BARNAME;
+    }
+
+}
+
+// $cola = new Beverage('black', 2);
+// $duvel = new Beer("Duvel", 8.5, "blond", 3.5);
+// echo Beverage::BARNAME;
+// echo "<br>";
+// echo $cola->getConst();
+// echo "<br>";
+// echo $duvel->message();
+// echo "<br>";
+echo Beverage::getAddress();
+echo "<br>";
+echo Beer::getAddress();
+
+
+
