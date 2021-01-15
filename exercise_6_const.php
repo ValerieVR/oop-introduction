7404acbdc5af66b860e6775717112489575f91b8
Valerie <akpotevalerie@gmail.com>
2021-01-15 11:18:24 +0100

    Add solution

--- exercise_6_const.php
+++ exercise_6_const.php
@@ -17,3 +17,56 @@ print the output of these functions on the screen.
 Make sure that every print is on a new line.
 Use typehinting everywhere!
 */
+
+class Beverage {
+
+    private $color, $price, $temperature;
+    const BARNAME = "Het Vervolg";
+
+    public function __construct(string $color, float $price, string $temperature = "cold") {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo() :string {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+
+    public function getConst() :string {
+        return "Deel 1: " . self::BARNAME;
+    }
+
+   
+}
+
+class Beer extends Beverage {
+    private $name, $alcoholpercentage;
+
+    public function __construct(string $name, float $alcoholpercentage, string $color, float $price) {
+        parent::__construct($color, $price);
+        $this->name = $name;
+        $this->alcoholpercentage = $alcoholpercentage;
+    }
+
+    public function getAlcoholpercentage() {
+        return $this->alcoholpercentage . "%";
+    }
+
+    public function message() :string {
+        return "Deel 2: " . self::BARNAME;
+    }
+
+}
+
+$cola = new Beverage('black', 2);
+$duvel = new Beer("Duvel", 8.5, "blond", 3.5);
+echo Beverage::BARNAME;
+echo "<br>";
+echo $cola->getConst();
+echo "<br>";
+echo $duvel->message();
+
+
+
+
